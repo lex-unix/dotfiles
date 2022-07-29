a4b7c685913fd178cec1e9d785850919f5de028f
Lex Nousagi <alexmiyin@gmail.com>
2022-07-29 16:30:36 +0300

    feat(fish): add peco and ghq

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,11 @@
-function fish_user_keybindings
+function fish_user_key_bindings
+  # peco
+  bind \cr peco_select_history # Bind for peco select history to Ctrl+R
+  bind \cf peco_change_directory # Bind for peco change directory to Ctrl+F
+
+  # vim-like
   bind \cl forward-char
 
-  #prevernt iterm2 from closing when typing CTRL-D 
+  # prevent iterm2 from closing when typing Ctrl-D (EOF)
   bind \cd delete-char
 end
