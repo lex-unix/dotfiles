ca7cb117516710037412e5b75c783873034e5569
Lex Nousagi <alexmiyin@gmail.com>
2022-09-18 20:46:12 +0300

    fix(nvim): use my username as module to avoid namespace collisions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
-require('base')
-require('highlights')
-require('maps')
-require('plugins')
+require('lex-unix.base')
+require('lex-unix.highlights')
+require('lex-unix.maps')
+require('lex-unix.plugins')
 
 local has = function(x)
   return vim.fn.has(x) == 1
@@ -10,7 +10,7 @@ local is_mac = has "macunix"
 local is_win = has "win32"
 
 if is_mac then
-  require('macos')
+  require('lex-unix.macos')
 end
 if is_win then
   require('windows')
