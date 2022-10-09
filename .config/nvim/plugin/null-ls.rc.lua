5434bffa362094b26f2793129872e38d48753362
Lex Nousagi <alexmiyin@gmail.com>
2022-10-09 17:32:09 +0300

    feat(nvim): use `null-ls` for rust formatting

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -10,6 +10,7 @@ null_ls.setup {
     }),
     null_ls.builtins.diagnostics.fish,
     null_ls.builtins.formatting.autopep8,
+    null_ls.builtins.formatting.rustfmt,
   },
   on_attach = function(client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
