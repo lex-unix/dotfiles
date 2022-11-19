0abbb5e4d9f2c0dd63cec58f5affb99f5233c61a
Lex Nousagi <alexmiyin@gmail.com>
2022-11-19 02:06:28 +0200

    feat(nvim): use `prettierd` to format `prisma` files

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -14,7 +14,9 @@ end
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettierd.with({
+      extra_filetypes = { "prisma" },
+    }),
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
