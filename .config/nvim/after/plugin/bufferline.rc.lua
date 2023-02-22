1dbdbad38f5aed33ce9902382a865d05c7fda168
Lex Nousagi <alexmiyin@gmail.com>
2023-02-22 14:06:47 +0200

    feat(nvim): rose-pine theme

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -2,34 +2,34 @@ local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
 bufferline.setup({
-  options = {
-    mode = "tabs",
-    separator_style = 'slant',
-    always_show_bufferline = false,
-    show_buffer_close_icons = false,
-    show_close_icon = false,
-    color_icons = true
-  },
-  highlights = {
-    separator = {
-      fg = '#073642',
-      bg = '#002b36',
+    options = {
+        mode = "tabs",
+        separator_style = 'slant',
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        color_icons = true
     },
-    separator_selected = {
-      fg = '#073642',
+    highlights = {
+        separator = {
+            fg = '#1f1d2e',
+            bg = '#21202e'
+        },
+        separator_selected = {
+            fg = '#1f1d2e'
+        },
+        background = {
+            fg = '#6e6a86',
+            bg = '#21202e'
+        },
+        buffer_selected = {
+            fg = '#e0def4',
+            bold = true,
+        },
+        fill = {
+            bg = '#1f1d2e'
+        }
     },
-    background = {
-      fg = '#657b83',
-      bg = '#002b36'
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true,
-    },
-    fill = {
-      bg = '#073642'
-    }
-  },
 })
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
