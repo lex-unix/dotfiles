7371341ae1b3963323af1c41a3b9a1df1a24b509
Lex Nousagi <alexmiyin@gmail.com>
2023-03-23 14:02:05 +0200

    feat(nvim): hide `tmux` status line when entering zen mode

--- .config/nvim/after/plugin/zen-mode.rc.lua
+++ .config/nvim/after/plugin/zen-mode.rc.lua
@@ -2,6 +2,9 @@ local status, zenMode = pcall(require, "zen-mode")
 if (not status) then return end
 
 zenMode.setup {
+  plugins = {
+    tmux = true
+  }
 }
 
 vim.keymap.set('n', '<C-w>o', '<cmd>ZenMode<cr>', { silent = true })
