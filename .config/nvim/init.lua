0c28dbc365de45b07c25c54d0b7574326ccfac64
Lex Nousagi <alexmiyin@gmail.com>
2023-07-19 17:22:39 +0300

    chore(nvim): remove conditional module loading as it's no longer used

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,16 +2,3 @@ require('lex-unix.base')
 require('lex-unix.highlights')
 require('lex-unix.maps')
 require('lex-unix.plugins')
-
-local has = function(x)
-  return vim.fn.has(x) == 1
-end
-local is_mac = has "macunix"
-local is_win = has "win32"
-
-if is_mac then
-  require('lex-unix.macos')
-end
-if is_win then
-  require('windows')
-end
