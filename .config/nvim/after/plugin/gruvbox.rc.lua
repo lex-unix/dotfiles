83b7d5d4bf95556e4d514acfead5039ae91ed029
Lex Nousagi <alexmiyin@gmail.com>
2023-08-30 22:44:35 +0300

    fix(nvim): change highlights for floats

--- .config/nvim/after/plugin/gruvbox.rc.lua
+++ .config/nvim/after/plugin/gruvbox.rc.lua
@@ -8,3 +8,10 @@ vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_float_style = 'dim'
 
 vim.cmd('colorscheme gruvbox-material')
+
+-- reset highlights for lsp-saga diagnostics and floats
+vim.api.nvim_set_hl(0, 'DiagnosticError', { link = 'Red' })
+vim.api.nvim_set_hl(0, 'DiagnosticHint', { link = 'Green' })
+vim.api.nvim_set_hl(0, 'DiagnosticWarn', { link = 'Yellow' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none', fg = '#d4be98' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none', fg = '#d4be98' })
