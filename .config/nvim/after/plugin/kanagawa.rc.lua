d29459121825178ea677c9ae43bfed78c959025c
Lex Nousagi <alexmiyin@gmail.com>
2023-09-01 14:29:02 +0300

    fix(nvim): comment out unused colorschemes

--- .config/nvim/after/plugin/kanagawa.rc.lua
+++ .config/nvim/after/plugin/kanagawa.rc.lua
@@ -1,32 +1,32 @@
-local status, kanagawa = pcall(require, 'kanagawa')
-if not status then return end
-
-kanagawa.setup({
-	transparent = true,
-	theme = 'dragon',
-	colors = {
-		theme = {
-			all = {
-				ui = {
-					bg_gutter = 'none',
-				},
-			},
-		},
-	},
-	overrides = function(colors)
-		local theme = colors.theme
-		return {
-			NormalFloat = { bg = 'none' },
-			FloatBorder = { bg = 'none' },
-			FloatTitle = { bg = 'none' },
-
-			TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = 'none' },
-			TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = 'none' },
-			TelescopePreviewBorder = { fg = theme.ui.bg_dim, bg = 'none' },
-
-			MasonNormal = { fg = theme.ui.fg_dim },
-		}
-	end,
-})
-
-vim.cmd('colorscheme kanagawa')
+-- local status, kanagawa = pcall(require, 'kanagawa')
+-- if not status then return end
+--
+-- kanagawa.setup({
+-- 	transparent = true,
+-- 	theme = 'dragon',
+-- 	colors = {
+-- 		theme = {
+-- 			all = {
+-- 				ui = {
+-- 					bg_gutter = 'none',
+-- 				},
+-- 			},
+-- 		},
+-- 	},
+-- 	overrides = function(colors)
+-- 		local theme = colors.theme
+-- 		return {
+-- 			NormalFloat = { bg = 'none' },
+-- 			FloatBorder = { bg = 'none' },
+-- 			FloatTitle = { bg = 'none' },
+--
+-- 			TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = 'none' },
+-- 			TelescopeResultsBorder = { fg = theme.ui.bg_p1, bg = 'none' },
+-- 			TelescopePreviewBorder = { fg = theme.ui.bg_p1, bg = 'none' },
+--
+-- 			MasonNormal = { fg = theme.ui.fg_dim },
+-- 		}
+-- 	end,
+-- })
+--
+-- vim.cmd('colorscheme kanagawa')
