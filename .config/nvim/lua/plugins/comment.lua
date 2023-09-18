ee3131723211246dff181075a077797542467866
Lex Miin <alexmiyin@gmail.com>
2023-09-18 22:05:37 +0300

    chore(nvim): run formatter

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,4 @@
 return {
-  'numToStr/Comment.nvim',
-  config = function()
-    require('Comment').setup()
-  end
+	'numToStr/Comment.nvim',
+	opts = {},
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,41 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      highlight = {
-        enable = true,
-        disable = {},
-      },
-      indent = {
-        enable = true,
-        disable = { 'python' },
-      },
-      sync_install = false,
-      auto_install = true,
-      ensure_installed = {
-        'tsx',
-        'toml',
-        'fish',
-        'json',
-        'yaml',
-        'css',
-        'html',
-        'lua',
-        'python',
-        'rust',
-        'go',
-        'markdown',
-        'markdown_inline',
-      },
-      autotag = {
-        enable = true,
-      },
-    })
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	config = function()
+		require('nvim-treesitter.configs').setup({
+			highlight = {
+				enable = true,
+				disable = {},
+			},
+			indent = {
+				enable = true,
+				disable = { 'python' },
+			},
+			sync_install = false,
+			auto_install = true,
+			ensure_installed = {
+				'tsx',
+				'toml',
+				'fish',
+				'json',
+				'yaml',
+				'css',
+				'html',
+				'lua',
+				'python',
+				'rust',
+				'go',
+				'markdown',
+				'markdown_inline',
+			},
+			autotag = {
+				enable = true,
+			},
+		})
 
-    local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-    parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
+		local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+		parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
 
-    vim.treesitter.language.register('markdown', 'mdx')
-  end,
+		vim.treesitter.language.register('markdown', 'mdx')
+	end,
 }
