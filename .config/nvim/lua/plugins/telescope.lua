8583fbbe3c6195791b1d910cbe69827275df92f3
Lex Miin <alexmiyin@gmail.com>
2023-09-18 21:53:05 +0300

    fix(nvim): plugins config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,93 +1,94 @@
 local function telescope_buffer_dir() return vim.fn.expand('%:p:h') end
 
 local file_browser_opts = {
-	path = '%:p:h',
-	cwd = telescope_buffer_dir(),
-	respect_gitignore = false,
-	hidden = true,
-	grouped = true,
-	previewer = false,
-	initial_mode = 'normal',
-	layout_config = { height = 40 },
+  path = '%:p:h',
+  cwd = telescope_buffer_dir(),
+  respect_gitignore = false,
+  hidden = true,
+  grouped = true,
+  previewer = false,
+  initial_mode = 'normal',
+  layout_config = { height = 40 },
 }
 
 return {
-	{
-		'nvim-telescope/telescope.nvim',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			'nvim-telescope/telescope-file-browser.nvim',
-			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-		},
-		lazy = true,
-		cmd = 'Telescope',
-		keys = {
-			{
-				'<leader>sf',
-				function() require('telescope.builtin').find_files({ no_ignore = false, hidden = true }) end,
-			},
-			{
-				'<leader>sg',
-				function() require('telescope.builtin').live_grep() end,
-			},
-			{
-				'<leader>sh',
-				function() require('telescope.builtin').help_tags() end,
-			},
-			{
-				'<leader>sd',
-				function() require('telescope.builtin').diagnostics() end,
-			},
-			{
-				'sf',
-				function() require('telescope').extensions.file_browser.file_browser(file_browser_opts) end,
-			},
-		},
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-file-browser.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    lazy = true,
+    cmd = 'Telescope',
+    keys = {
+      {
+        '<leader>sf',
+        function() require('telescope.builtin').find_files({ no_ignore = false, hidden = true }) end,
+      },
+      {
+        '<leader>sg',
+        function() require('telescope.builtin').live_grep() end,
+      },
+      {
+        '<leader>sh',
+        function() require('telescope.builtin').help_tags() end,
+      },
+      {
+        '<leader>sd',
+        function() require('telescope.builtin').diagnostics() end,
+      },
+      {
+        'sf',
+        function() require('telescope').extensions.file_browser.file_browser(file_browser_opts) end,
+      },
+    },
 
-		config = function()
-			local telescope = require('telescope')
-			local actions = require('telescope.actions')
-			local fb_actions = require('telescope').extensions.file_browser.actions
+    config = function()
+      local telescope = require('telescope')
+      local actions = require('telescope.actions')
+      local fb_actions = require('telescope').extensions.file_browser.actions
 
-			telescope.setup({
-				defaults = {
-					file_ignore_patterns = { '.git/', '%.jpeg', '%.jpg', '%.webp', '%.pdf' },
-					mappings = {
-						n = {
-							['q'] = actions.close,
-						},
-					},
-				},
-				extensions = {
-					file_browser = {
-						theme = 'dropdown',
-						-- disables netrw and use telescope-file-browser in its place
-						hijack_netrw = true,
-						mappings = {
-							-- your custom insert mode mappings
-							['i'] = {
-								['<C-w>'] = function() vim.cmd('normal vbd') end,
-							},
-							['n'] = {
-								-- your custom normal mode mappings
-								['N'] = fb_actions.create,
-								['h'] = fb_actions.goto_parent_dir,
-								['.'] = fb_actions.toggle_hidden,
-								['/'] = function() vim.cmd('startinsert') end,
-							},
-						},
-					},
-				},
-			})
+      telescope.setup({
+        defaults = {
+          sorting_strategy = 'ascending',
+          file_ignore_patterns = { '.git/', '%.jpeg', '%.jpg', '%.webp', '%.pdf' },
+          mappings = {
+            n = {
+              ['q'] = actions.close,
+            },
+          },
+        },
+        extensions = {
+          file_browser = {
+            theme = 'dropdown',
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              -- your custom insert mode mappings
+              ['i'] = {
+                ['<C-w>'] = function() vim.cmd('normal vbd') end,
+              },
+              ['n'] = {
+                -- your custom normal mode mappings
+                ['N'] = fb_actions.create,
+                ['h'] = fb_actions.goto_parent_dir,
+                ['.'] = fb_actions.toggle_hidden,
+                ['/'] = function() vim.cmd('startinsert') end,
+              },
+            },
+          },
+        },
+      })
 
-			telescope.load_extension('file_browser')
-			telescope.load_extension('fzf')
+      telescope.load_extension('file_browser')
+      telescope.load_extension('fzf')
 
-			-- vim.api.nvim_create_autocmd('VimEnter', {
-			-- 	callback = function()
-			-- 		if vim.fn.argv(0) == '' then telescope.extensions.file_browser.file_browser(file_browser_opts) end
-			-- 	end,
-			-- })
-		end,
-	},
+      -- vim.api.nvim_create_autocmd('VimEnter', {
+      -- 	callback = function()
+      -- 		if vim.fn.argv(0) == '' then telescope.extensions.file_browser.file_browser(file_browser_opts) end
+      -- 	end,
+      -- })
+    end,
+  },
 }
