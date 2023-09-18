29ead06bb120a9305364e5421684cebd9cc094a6
Lex Miin <alexmiyin@gmail.com>
2023-09-18 20:00:34 +0300

    feat(nvim): move from packer to lazy

---
+++ .config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    'hrsh7th/cmp-buffer',
+  },
+  {
+    'hrsh7th/cmp-nvim-lsp',
+  },
+  {
+    'hrsh7th/cmp-path',
+  },
+  {
+    'L3MON4D3/LuaSnip'
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require('cmp')
+      cmp.setup({
+
+        snippet = {
+          expand = function(args) require('luasnip').lsp_expand(args.body) end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.close(),
+          ['<CR>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+        formatting = {
+          format = require('lspkind').cmp_format({ with_text = false, maxwidth = 50 }),
+        },
+      })
+
+      vim.cmd([[
+        set completeopt=menuone,noinsert,noselect
+        highlight! default link CmpItemKind CmpItemMenuDefault
+      ]])
+    end,
+  },
+}
---
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,15 @@
+return {
+	'folke/zen-mode.nvim',
+	lazy = true,
+	keys = {
+		{ '<leader>z', '<cmd>ZenMode<CR>', silent = true, desc = 'Zen Mode' },
+	},
+	config = function()
+		require('zen-mode').setup({
+			plugins = {
+				tmux = true,
+			},
+		})
+		vim.keymap.set('n', '<leader>z', '<cmd>ZenMode<cr>', { silent = true })
+	end,
+}
