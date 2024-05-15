ec61b9b32e9935669f4b8ca8d938bd2ea743e124
Lex Miin <alexmiyin@gmail.com>
2024-05-15 16:42:57 +0300

    fix(tmux, fish): don't set `TERM` variable in shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g default_user ___
 
-set -gx TERM xterm-256color
+# set -gx TERM xterm-256color
 
 set -g FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always --line-range :500"
 set -g FZF_LEGACY_KEYBINDINGS 0
