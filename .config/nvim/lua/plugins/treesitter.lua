9bbba4fac2a0c86e76b56258dcfd6978d3669470
Lex Miin <alexmiyin@gmail.com>
2024-07-30 23:07:24 +0300

    chore(nvim): use setup for `nvim-ts-autotag`

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -29,7 +29,7 @@ return {
                 'markdown_inline',
             },
             autotag = {
-                enable = true,
+                enable = false,
             },
         })
 
--- .config/nvim/lua/plugins/ts-autotag.lua
+++ .config/nvim/lua/plugins/ts-autotag.lua
@@ -1,3 +1,4 @@
 return {
     'windwp/nvim-ts-autotag',
+    config = function() require('nvim-ts-autotag').setup() end,
 }
