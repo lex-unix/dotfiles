95fc761a6518eb7c1247dde81a7cd490fd326ba8
Lex Miin <alexmiyin@gmail.com>
2024-07-30 10:34:14 +0300

    fix: attach to existing session while outside of tmux

--- .config/fish/functions/tmux_sessionizer.fish
+++ .config/fish/functions/tmux_sessionizer.fish
@@ -14,11 +14,22 @@ function tmux_sessionizer
   set selected_name (basename "$selected" | tr . _)
   set tmux_running (pgrep tmux)
 
+  # tmux is not running
   if test -z $TMUX; and test -z $tmux_running
     tmux new-session -s $selected_name -c $selected
     return
   end
 
+  # tmux is running but not active
+  if test -z $TMUX; and test -n $tmux_running
+    if not tmux has-session -t $selected_name 2> /dev/null 
+      tmux new-session -s $selected_name -c $selected
+    else
+        tmux attach-session -t $selected_name
+    end
+  end
+
+  # tmux running and active
   if not tmux has-session -t $selected_name 2> /dev/null 
     tmux new-session -ds $selected_name -c $selected
   end
