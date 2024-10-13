72fae70af8aaa80869b02a54cf5ac9d54e261d3f
Lex Miin <alexmiyin@gmail.com>
2024-10-13 16:06:11 +0300

    feat(nvim): pkl parser for treestiter

---
+++ nvim/lua/plugins/pkl.lua
@@ -0,0 +1,13 @@
+return {
+    'https://github.com/apple/pkl-neovim',
+    lazy = true,
+    event = {
+        'BufReadPre *.pkl',
+        'BufReadPre *.pcf',
+        'BufReadPre PklProject',
+    },
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+    },
+    build = function() vim.cmd('TSInstall! pkl') end,
+}
