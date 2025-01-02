583f893f416d92783d9aba6393ef90c918a1a246
Lex Miin <alexmiyin@gmail.com>
2025-01-02 20:02:37 +0200

    feat(nvim): add comments.nvim plugin

--- nvim/lua/plugins/dressing.lua
+++ nvim/lua/plugins/dressing.lua
@@ -1,5 +1,15 @@
 return {
-    'stevearc/dressing.nvim',
-    event = 'VeryLazy',
-    opts = {},
+    {
+        'stevearc/dressing.nvim',
+        event = 'VeryLazy',
+        opts = {},
+    },
+    {
+        'folke/todo-comments.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+        keys = {
+            { '<leader>st', '<CMD>TodoTelescope<CR>', desc = 'Search Todos' },
+        },
+        opts = {},
+    },
 }
