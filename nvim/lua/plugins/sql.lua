887c14d9964fb30bc262c3590eb579dee70d7dbe
Lex Miin <alexmiyin@gmail.com>
2025-03-05 17:56:03 +0200

    feat(nvim): add sql binding pattern for dadbod

--- nvim/lua/plugins/sql.lua
+++ nvim/lua/plugins/sql.lua
@@ -10,5 +10,8 @@ return {
         'DBUIAddConnection',
         'DBUIFindBuffer',
     },
-    init = function() vim.g.db_ui_use_nerd_fonts = 1 end,
+    init = function()
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.db_ui_bind_param_pattern = [[\$\d\+\|:\w\+]]
+    end,
 }
