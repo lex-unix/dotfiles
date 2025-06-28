9d911895e6a0b4aae8b463beb42b75e433e696db
Lex Miin <alexmiyin@gmail.com>
2025-06-28 22:36:53 +0300

    fix(nvim): `DisableFormat` command not working

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -31,6 +31,7 @@ return {
     opts = {
         notify_on_error = false,
         format_on_save = function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
             local disable_filetypes = { c = true, cpp = true }
             local lsp_format_opt
             if disable_filetypes[vim.bo[bufnr].filetype] then
